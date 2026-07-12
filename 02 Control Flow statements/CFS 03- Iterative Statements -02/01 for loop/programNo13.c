cbb0aee1597c0d6ebf1a08b7bc92fa7527753f5b
Gian <smourya1046@gmail.com>
2026-07-12 16:47:55 +0530

    modifying the folder

--- 02 Control Flow statements/CFS 03- Iterative Statements -02/01 for loop/programNo13.c
+++ 02 Control Flow statements/CFS 03- Iterative Statements -02/01 for loop/programNo13.c
@@ -3,13 +3,13 @@
 int main()
 {
     int n;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i=i*3)
     {
-        for (int j = i; j <= 3 * i; j++)
+        for (int j = i; j <= n; j++)
         {
             printf("mew mew \n");
         }
     }
-
+    
     return 0;
 }
---
+++ 02 Control Flow statements/CFS 03- Iterative Statements -02/01 for loop/programNo14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    while (i <= 5) // this means , the loop will go till , 6
+    {
+        printf("%d\n", i);
+       i++;
+    }
+
+    return 0;
+}
