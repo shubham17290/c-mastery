a1d7a1e1b691e24d40a7cf095bea56078d9aa04e
Gian <smourya1046@gmail.com>
2026-07-15 08:55:57 +0530

    structuring the for and while loop structure properly to solve the problems on it

--- 02 Control Flow statements/CFS 05 - Iterative Statemets More About Loops/02 Problem solving on For loop/programNo _02.c
+++ 02 Control Flow statements/CFS 05 - Iterative Statemets More About Loops/02 Problem solving on For loop/programNo _02.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int n;
+
+    printf("Enter value: ");
+    scanf("%d", &n);
+
+    printf("Value = %d\n", n);
+    for (int i = 0; i < n; i++) {
+    }
 
     return 0;
 }
---
+++ 02 Control Flow statements/CFS 05 - Iterative Statemets More About Loops/03 Problem solving on while loop/programNo _01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   int n;
+
+   printf("Enter value: ");
+   scanf("%d", &n);
+
+   printf("Value = %d\n", n);
+
+   while (34+43>42) // roughly assigned condition
+   {
+    /* code */
+   }
+
+
+
+    return 0;
+}
