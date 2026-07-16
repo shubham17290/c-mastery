3467f27aaba638abd54f207f9d3f7126cf506346
Gian <smourya1046@gmail.com>
2026-07-16 09:16:29 +0530

    upward and downward triangle left ended programs ,  increaseing and decreasing order

---
+++ 02 Control Flow statements/CFW 06 - Iterative Statements More about Loops part 02/01 class work/02 Patterns printing program/programNo15.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    printf("Enter number of row's:  ");
+
+    scanf("%d", &n);
+
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= row; col++) {
+            printf("%d", row);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
