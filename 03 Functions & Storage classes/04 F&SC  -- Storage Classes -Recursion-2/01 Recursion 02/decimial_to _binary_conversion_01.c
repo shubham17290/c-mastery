f3169cb31d26819d57aea3ea77896dfb3a85274c
Gian <smourya1046@gmail.com>
2026-07-23 12:04:57 +0530

    understanding the base case

---
+++ 03 Functions & Storage classes/04 F&SC  -- Storage Classes -Recursion-2/01 Recursion 02/decimial_to _binary_conversion_01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void f(int n) {
+    // if () { // base case --> works for the small numbers
+
+    // } else { // large or main case --> it include's the recursion call  and have the recursion
+             // function that helps in terms calling the function again and again
+    // }
+}
+void main() {}
