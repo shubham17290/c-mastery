456c443008ed94196a7a6e7e457b1a154c974479
Gian <smourya1046@gmail.com>
2026-07-24 11:10:53 +0530

    2nd commit "in the Tower of hanoi"

---
+++ 03 Functions & Storage classes/05 F&SC  -- Storage Classes -Recursion-3/classwork/TOH_01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void TOH(int n, char source, char dest, char aux) {
+    if (n == 1) {
+        printf("%c-->%c\n", source, dest);
+        return;
+    } else {
+        TOH(n - 1, source, aux, dest);
+        printf("%c-->%c\n", source, dest);
+        TOH(n - 1, aux, dest, source);
+    }
+}
+int main() {
+    int n;
+    printf("Enter  number of disks = \n");
+    scanf("%d", &n);
+    TOH(n, 'A', 'C', 'B');
+    return 0;
+}
