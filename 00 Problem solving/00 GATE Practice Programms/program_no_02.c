9f3634aac7aa2d2e9ed18aaab1ce62f66941b159
Gian <smourya1046@gmail.com>
2026-07-25 10:23:25 +0530

    2nd commit "understanding the cyclic property of the character data types "

---
+++ 00 Problem solving/00 GATE Practice Programms/program_no_02.c
@@ -0,0 +1,9 @@
+//  Print the characters integer value to your external terminal
+#include <stdio.h>
+
+int main() {
+    char ch = 129;
+    printf("%d\n", ch);
+
+    return 0;
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/program_no_04.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    char ch = 129;
+    printf("%d\n", ch);
+
+    return 0;
+}
