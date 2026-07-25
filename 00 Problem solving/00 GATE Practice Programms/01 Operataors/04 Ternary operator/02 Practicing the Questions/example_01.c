db01739caa0da39c2817bcc7c437f24e54b69f05
Gian <smourya1046@gmail.com>
2026-07-25 16:00:56 +0530

    21th commit "example no  1 and 3 done "

--- 00 Problem solving/00 GATE Practice Programms/01 Operataors/04 Ternary operator/02 Practicing the Questions/example_01.c
+++ 00 Problem solving/00 GATE Practice Programms/01 Operataors/04 Ternary operator/02 Practicing the Questions/example_01.c
@@ -3,7 +3,9 @@
 
 int main() {
     int a = 2, b = 2, c = 0, d = 2, e;
-    e = a++ && b++ && c++ || d++;
+    e = a++ && b++ && c++ ||
+        d++; // there is the crux this expression is written to remind you that the concept called :
+             // short circuit evaluation in the logical or operator
     printf("%d\n", e);
 
     return 0;
