b6ae17ddaa714211cc0cad308e3aa02066ab573b
Gian <smourya1046@gmail.com>
2026-07-25 14:52:07 +0530

    15th commit " bitwise and and or operator"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 Operataors/02 Logical oper and practice questions/05 Bit-wise operators/program_01.c
@@ -0,0 +1,10 @@
+//  program no 01 : and (&) opr
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 17, c;
+    c = a & b;
+    printf("%d\n", c);
+
+    return 0;
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/01 Operataors/02 Logical oper and practice questions/05 Bit-wise operators/program_02.c
@@ -0,0 +1,11 @@
+//  bit-wise = or operator
+
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 17, c;
+    c = a | b;
+    printf("%d\n", c);
+
+    return 0;
+}
