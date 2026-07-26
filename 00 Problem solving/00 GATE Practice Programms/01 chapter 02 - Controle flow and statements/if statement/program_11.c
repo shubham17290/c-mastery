ed18aa3017cf7b6e812dca20e8c9f85a21c533ba
Gian <smourya1046@gmail.com>
2026-07-26 07:19:59 +0530

    5th commit " keep doing practice one more on the if "

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // if () {
+    //     printf("Hi shubham maurya and future GATE cs&it 2027 AIR 01 \n");
+    // }
+
+    return 0;
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    if (12 + 3 * 4) {
+        printf("3\n");
+        printf("32\n");
+    }
+
+    return 0;
+}
