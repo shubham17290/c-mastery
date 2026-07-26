ab08044d365ed802c1406aebc350551ee83d4988
Gian <smourya1046@gmail.com>
2026-07-26 07:10:19 +0530

    3rd commit " keep doing if practice"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_07.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int i = 4;
+    if (i == 4)
+        printf("Pankaj\n");
+
+    return 0;
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    if (i = 4) // here i is assigned as the new value
+    {
+        printf("SDE shubham maurya and the GATE CS&IT air 01 of 2027 \n");
+    }
+
+    return 0;
+}
