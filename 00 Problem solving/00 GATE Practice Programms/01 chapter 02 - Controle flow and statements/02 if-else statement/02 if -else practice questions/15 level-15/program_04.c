62694f37cdf9eac1bc1bc5c4b7d0cd1b6ec7063c
Gian <smourya1046@gmail.com>
2026-07-26 15:01:34 +0530

    35th commit  " the largest among 4 using ternary operator is done"

--- 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/15 level-15/program_04.c
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/15 level-15/program_04.c
@@ -1,14 +1,14 @@
-// 35th commit " largest among 3 is done using the ternary operator"
-//  wap to print the largest among three numbers :
+// 35th commit " largest among 4 is done using the ternary operator"
+//  wap to print the largest among four numbers :
 #include <stdio.h>
 
 int main() {
-    int a, b, c, largest;
+    int a, b, c, d, largest;
 
     printf("Enter value: ");
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d %d", &a, &b, &c, &d);
 
-    largest = (a > b && a > c) ? a : (b > c) ? b : c;
+    largest = (a > b && a > c) ? a : (b > c && b > d) ? b : (c > d) ? c : d;
     printf(" largest value among all  is = %d\n", largest);
 
     return 0;
