9f72baec8dc63da0b1fc20e12cc5b043edb29880
Gian <smourya1046@gmail.com>
2026-07-26 06:58:08 +0530

    1st commit "of the day  understanding the if statement"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+    printf("Start\n");
+    if (2 < 3) {
+        printf("2\n");
+        printf("3\n");
+    }
+    printf("End\n");
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 3;
+    if (i + 2) {
+
+        printf("hello\n");
+        printf("kaise ho\n");
+    }
+    printf("Sab mast hai bhai \n");
+
+    return 0;
+}
