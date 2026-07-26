b60f9ee67724fd6c4f3842706646ca1473495378
Gian <smourya1046@gmail.com>
2026-07-26 21:32:44 +0530

    40th commit "starting the concept of the nested for loop"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/lecture 03 practice and problem solving/nested for loop/program_01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter value: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i < n; i++) {
+        printf("i is %d\n", i);
+    }
+
+    return 0;
+}
