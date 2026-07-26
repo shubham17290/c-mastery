f0f336999dae4bdadc7e20448dc60055edbefb68
Gian <smourya1046@gmail.com>
2026-07-26 13:41:40 +0530

    25th commit " level 7 is done "

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/07 level-07/program_01.c
@@ -0,0 +1,21 @@
+
+//  Level 7 (Bitwise vs Logical) ⭐⭐⭐⭐
+//  Q: 31
+
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    printf("Enter value: ");
+    scanf("%d %d", &x, &y);
+
+    if (x & y) {
+        printf("if has non- zero value %d\n", x & y);
+    }
+
+    else {
+        printf("if has  zero value %d\n", x & y);
+    }
+    return 0;
+}
