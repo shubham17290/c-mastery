f512feb5a7177d741fc6e539b4b46cfb01647cc5
Gian <smourya1046@gmail.com>
2026-07-26 10:30:44 +0530

    11th commit  "solved two questions "

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/01 if-else codes for logic building/program_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    if (2 + 3 - 6) {
+        printf("Hello\n");
+        printf("Dosto\n");
+    } else {
+        printf("Maza\n");
+        printf("Aagaya\n");
+    }
+
+    return 0;
+}
