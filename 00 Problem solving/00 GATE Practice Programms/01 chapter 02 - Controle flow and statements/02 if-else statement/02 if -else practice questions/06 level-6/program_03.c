263da1dee80f6f45143109e6635e44e4d9e0f184
Gian <smourya1046@gmail.com>
2026-07-26 13:22:01 +0530

    23th commit " Level - 6 is done"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/06 level-6/program_03.c
@@ -0,0 +1,17 @@
+
+//  Q : 28  Level 6 (Operator Precedence) ⭐⭐⭐⭐⭐
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int c = 2;
+    int d = 6;
+    if (a > b && c || d) {
+        printf("If have non-zero value\n");
+    } else {
+        printf("If have zero value\n");
+    }
+
+    return 0;
+}
