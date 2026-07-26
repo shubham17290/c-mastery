cad616f94b5ddc77363e34863c64a181a464c4ca
Gian <smourya1046@gmail.com>
2026-07-26 07:14:59 +0530

    4th commit  " keep doing if + loop "

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_09.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    if (0)
+        printf("Mew Mew SDE - 3\n");
+
+    return 0;
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    if (12.338) {
+        int i;
+        for (i = 1; i <= 12; i++) {
+            printf("Hi shubham maurya and future GATE cs&it 2027 AIR 01 \n");
+        }
+        printf("Loop completely execute ho gya hai bhai And now i is %d\n",
+               i); // here i will be = 13
+    }
+
+    return 0;
+}
