b0ff85c08ee531959ee84d05a80d52c1e1ba3098
Gian <smourya1046@gmail.com>
2026-07-26 12:44:48 +0530

    17th commit " just solved two problems of the if-else statement"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/02 level-2/program_01.c
@@ -0,0 +1,12 @@
+// assignment vs equality
+// Question no : 01
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    if (a = 5) {
+        printf("hare ram hare ram\n");
+    }
+
+    return 0;
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/02 level-2/program_02.c
@@ -0,0 +1,11 @@
+// Q - 02 : assignment
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    if (a = 0) {
+        printf("Chal gya bhai \n");
+    }
+
+    return 0;
+}
