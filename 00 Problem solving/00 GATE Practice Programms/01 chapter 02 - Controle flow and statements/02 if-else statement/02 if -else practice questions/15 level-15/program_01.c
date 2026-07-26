de0b35922d1affee4a9461568d2c5bf3047ed9b4
Gian <smourya1046@gmail.com>
2026-07-26 14:39:36 +0530

    32th commit " largest among 5 is done using the if statement"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/15 level-15/program_01.c
@@ -0,0 +1,22 @@
+//  wap to print the largest among three numbers
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter value: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c) {
+        printf("a is the largest %d\n", a);
+    }
+    if (b > a && b > c) {
+        printf("b is the largest %d\n", b);
+    }
+    if (c > a && c > b) {
+        printf("c is the largest %d\n", c);
+    }
+
+    return 0;
+}
