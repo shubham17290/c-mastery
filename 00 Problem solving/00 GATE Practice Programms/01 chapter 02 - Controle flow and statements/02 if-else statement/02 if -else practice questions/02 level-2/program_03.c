c885d782343fa20f39d51259d9c207ad151ebea6
Gian <smourya1046@gmail.com>
2026-07-26 12:49:23 +0530

    18th commit "just done with the level two concept"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/02 level-2/program_03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    //  Question no : 09-- equality
+    int a = 5;
+    if (a == 5) {
+        printf("Hmm meri jaan !\n");
+    }
+
+    return 0;
+}
---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/02 if-else statement/02 if -else practice questions/02 level-2/program_04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    if (a != 5) {
+        printf("Bhai ye kya condition diya hai tu \n");
+    }
+
+    return 0;
+}
