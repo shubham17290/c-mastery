b29bc683a5a3fb91d956c8c551537b2679235703
Gian <smourya1046@gmail.com>
2026-07-26 07:05:54 +0530

    2nd commit " keep doing CFS"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/if statement/program_06.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hi\n");
+    if (!2) {
+        printf("Bhagwan\n");
+        printf("Baccha lo \n");
+    }
+    printf("Is ravan se \n");
+
+    return 0;
+}
