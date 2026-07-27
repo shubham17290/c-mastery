75005f1532f9f7bdbd261e4c125197aa5985ac41
Gian <smourya1046@gmail.com>
2026-07-27 10:28:19 +0530

    4th commit   " solved the switch case problem"

---
+++ 00 Problem solving/00 GATE Practice Programms/01 chapter 02 - Controle flow and statements/lecture 03 practice and problem solving/04 jumping statements/program_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i = 2;
+    switch (i) {
+    case 2:
+        printf("2\n");
+        break;
+
+    case 4:
+        printf("4\n");
+        break;
+    }
+
+    return 0;
+}
