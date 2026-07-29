05a3eff79c9b0e41df7ca0d2ebcc085ce86a7800
Gian <smourya1046@gmail.com>
2026-07-29 10:42:17 +0530

    8th commit  " pointer arithmatic in the 2-D array"

---
+++ 04 Arrays and  Pointers/lec 02/1-D Array/02 Practice Problems/2-D array/important code's/pointer_arithmatic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {10, 20, 30};
+
+    printf("%d\n", *(a + 1));
+    printf("%d\n", a[1]);
+    printf("%d\n", 1 [a]);
+
+    return 0;
+}
---
+++ 04 Arrays and  Pointers/lec 02/1-D Array/02 Practice Problems/2-D array/problem_02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {10, 20, 30};
+
+    printf("%u\n", a);
+    printf("%u\n", &a[0]);
+
+    return 0;
+}
