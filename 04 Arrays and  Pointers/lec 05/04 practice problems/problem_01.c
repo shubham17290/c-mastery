8a3398c016310c748a17707e8a35cf2561f000f9
Gian <smourya1046@gmail.com>
2026-07-31 07:45:43 +0530

    01 commit " practicing the problems"

---
+++ 04 Arrays and  Pointers/lec 05/04 practice problems/problem_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a[4] = {10, 20, 30, 40};
+
+    fun(a);
+    printf("%d %d\n", a[0], a[1]);
+
+    return 0;
+}
+
+int fun(int *p) {
+    ++p;
+    ++*p;
+}
