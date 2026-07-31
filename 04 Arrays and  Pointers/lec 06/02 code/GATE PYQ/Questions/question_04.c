cf93f5c7e1db1e4ed4ae2c2b7f22dfa33b501d5f
Gian <smourya1046@gmail.com>
2026-07-31 11:20:48 +0530

    8th commit " problem solving"

---
+++ 04 Arrays and  Pointers/lec 06/02 code/GATE PYQ/Questions/question_04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a[4] = {10, 20, 30, 40};
+
+    // Array of 4 pointers pointing to elements of 'a'
+    int *P[4] = {a + 3, a + 2, a + 1, a};
+    int y;
+
+    // Pointer pre-decrement and subtraction
+    y = --P[0] - P[1];
+
+    printf("%d\n", y);
+    printf("%d\n", *P[0]);
+
+    return 0;
+}
