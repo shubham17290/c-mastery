a0b7edf473430044239bd65cfb18dca81a2a5652
Gian <smourya1046@gmail.com>
2026-08-01 09:37:00 +0530

    9th commit ' practicing the examples "

---
+++ 05 Strings/lec 01/02 Class Code/example_01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+
+    char name[10] = "shubham ";
+    printf("%s\n", name);
+
+    return 0;
+}
---
+++ 05 Strings/lec 01/02 Class Code/example_02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[4] = {1, 2, 3, 4};
+    return 0;
+}
