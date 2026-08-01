3a6230036ba2f69613e896d775f7e344df4bd0a7
Gian <smourya1046@gmail.com>
2026-08-01 12:13:12 +0530

    20th commit "comparsion in the two strings characters"

---
+++ 05 Strings/lec 02/02 Class Codes/example_05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    //  "Gate[1] ==> *("Gate" + 1) ==> 'a'"
+    //  " Game"[1] ==> *(" Game" + 1) ==> 'a'"
+    if ("Gate"[1] == "Game"[1]) {
+        printf("jai ho\n");
+    } else {
+        printf("shubham ki\n");
+    }
+
+    return 0;
+}
