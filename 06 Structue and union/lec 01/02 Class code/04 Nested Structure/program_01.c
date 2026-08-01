4a8c977cdd985c7a61a6b83dd0214ae6c220d70b
Gian <smourya1046@gmail.com>
2026-08-01 18:30:13 +0530

    36th commit " DOB concept int he Structure "

---
+++ 06 Structue and union/lec 01/02 Class code/04 Nested Structure/program_01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+// Structure Definition for Date of Birth
+struct date_of_birth {
+    int day;
+    int month;
+    int year;
+};
+
+// Structure Definition for Student
+struct stu {
+    int roll;
+    char name[20];
+
+    // Nested Structure
+    struct date_of_birth DOB;
+};
+
+int main() {
+    struct stu s;
+
+    // Student Details
+    s.roll = 10;
+    strcpy(s.name, "Pankaj");
+
+    // Date of Birth
+    s.DOB.day = 2;
+    s.DOB.month = 3;
+    s.DOB.year = 1982;
+
+    // Display Output
+    printf("Roll Number : %d\n", s.roll);
+    printf("Name        : %s\n", s.name);
+
+    printf("Date of Birth : %02d/%02d/%04d\n", s.DOB.day, s.DOB.month, s.DOB.year);
+
+    return 0;
+}
