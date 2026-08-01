3dfcd1f8ef406025bb55db96993c4898dcc0dc25
Gian <smourya1046@gmail.com>
2026-08-01 17:23:39 +0530

    31th commit "structure validity check "

--- 06 Structue and union/lec 01/02 Class code/structure_01.c
+++ 06 Structue and union/lec 01/02 Class code/structure_01.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
 struct student {
     int roll;
     char name[20];
 }; // this is new-datatype structure student --> template / structure or blueprint
+void main() {
+    struct student s1, s2;
+    s1.roll = 10;
+    s2.roll = 20;
+    strcpy(s1.name, "Ravi");
+    strcpy(s2.name, "Ramesh");
+}
