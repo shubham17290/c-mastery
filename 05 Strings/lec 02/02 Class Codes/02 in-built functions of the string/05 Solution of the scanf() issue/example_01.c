29adb66f1c12851d7251494807a8057a774d3d2c
Gian <smourya1046@gmail.com>
2026-08-01 13:57:48 +0530

    27th commit "gets -> the solution of the scanf()"

---
+++ 05 Strings/lec 02/02 Class Codes/02 in-built functions of the string/05 Solution of the scanf() issue/example_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int i;
+
+    char arr[20];
+    printf("Enter your name: ");
+    scanf("%s", arr);
+
+    printf("%s\n", arr);
+
+    return 0;
+}
---
+++ 05 Strings/lec 02/02 Class Codes/02 in-built functions of the string/05 Solution of the scanf() issue/example_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int i;
+
+    char arr[20];
+    //  to handle the scanf() issue, we can use the following approach:gets()
+    printf("Enter your name: ");
+    gets(arr); // this is the solution of the scanf() issue, but this is not a good approach because
+               // it can cause buffer overflow. So, we can use the following approach: fgets()
+    // fgets(arr, sizeof(arr), stdin); // this is the solution of the scanf()
+
+    printf("%s\n", arr);
+
+    return 0;
+}
