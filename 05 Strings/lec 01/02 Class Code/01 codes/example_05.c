0384f53baebc0e922423fb7bc2a4ad654444f4fa
Gian <smourya1046@gmail.com>
2026-08-01 10:50:36 +0530

    11th commit " array vs string programs"

---
+++ 05 Strings/lec 01/02 Class Code/01 codes/example_05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    // this is a simple character array in c
+    char name[] = {'s', 'h', 'u', 'b', 'h', 'a', 'm', '\0'};
+    printf("%s\n", name);
+
+    return 0;
+}
---
+++ 05 Strings/lec 01/02 Class Code/03 String VS array/02_string_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char *ptr = " shubham ";
+    ptr = "pankaj";
+    printf("%s\n", ptr);
+    printf("%s\n", *ptr);
+
+    return 0;
+}
