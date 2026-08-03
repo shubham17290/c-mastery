aaa2c79e81454a373231b274662738e392d03c1f
Gian <smourya1046@gmail.com>
2026-08-03 11:05:33 +0530

    5th commit " increment operator is done "

---
+++ 00 Problem solving/01 Leetcode problems/02 Practice and Rivision/Module 03 operators & precedence/04 Modify Operator/increment_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//  pre-increment operator
+int main() {
+    int a = 5;
+    printf("%d\n", ++a);
+    printf("%d\n", a);
+
+    return 0;
+}
---
+++ 00 Problem solving/01 Leetcode problems/02 Practice and Rivision/Module 03 operators & precedence/04 Modify Operator/increment_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//  post increment operator
+int main() {
+    int a = 5;
+    printf("%d\n", a++); // a = 5
+    // a++ -->  a= a+1
+    printf("%d\n", a);
+
+    return 0;
+}
