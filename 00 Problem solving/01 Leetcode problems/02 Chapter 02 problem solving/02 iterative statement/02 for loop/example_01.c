2c2d1315b86afd50327364f5e1b741969cb4b1ef
Gian <smourya1046@gmail.com>
2026-08-04 16:27:48 +0530

    6th commit ' for loop execution practice "

---
+++ 00 Problem solving/01 Leetcode problems/02 Chapter 02 problem solving/02 iterative statement/02 for loop/example_01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    for (10; 11; 12) {
+        printf("Gate CS&IT AIR 01 and Software engineer  at google \n");
+    }
+
+    return 0;
+}
---
+++ 00 Problem solving/01 Leetcode problems/02 Chapter 02 problem solving/02 iterative statement/02 for loop/example_02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    for (0; 1; 0) {
+        printf("Gate CS&IT AIR 01 and Software engineer  at google \n");
+    }
+
+    return 0;
+}
